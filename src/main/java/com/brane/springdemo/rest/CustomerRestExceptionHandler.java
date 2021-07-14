606c5ac4a2e48f39bc7be7c12a7484d9e0d056a7
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-07-14 13:14:33 +0200

    We added one more exception Handler.THIS IS GENERIC EXCEPTION HANDLER and this handler can catch any exception (catch all) except this exception CustomerNotFoundException.Example:api/customers/dhdjs

--- src/main/java/com/brane/springdemo/rest/CustomerRestExceptionHandler.java
+++ src/main/java/com/brane/springdemo/rest/CustomerRestExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+
 //4.GLOBAL EXCEPTION HANDER
 //We will add here annotation @ControllerAdvice, because when we have more REST services,
 //then we need to add this 2 exception handler in every RestController class.
@@ -19,6 +20,7 @@ public class CustomerRestExceptionHandler {
 	//THIS EXCEPTION HANDLER CAN TO HANDLE OR CATCH ONLY THIS EXCEPTION CustomerNotFoundException
 	//RESPONSE ENTITY IS WRAPPER CLASS FOR CustomerErrorResponse OBJECT
 	//RESPONSE TYPE IS TYPE OF THE RESPONSE BODY-CustomerErrorResponse
+	//example:when we enter-api/employee/999 then will be called this exception
 	@ExceptionHandler
 	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exc) {
 		
@@ -32,11 +34,24 @@ public class CustomerRestExceptionHandler {
 		//ERROR IS BODY OF THE RESPONSE, AND HttpStatus.NOT_FOUND IS STATUS CODE FOR THIS RESPONSE
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 		
-		//JACKSON WILL TAKE THIS JAVA POJO AND CONVERT IT TO JSON AND SHOW US IN OUR REST CLIENT
-		//OUR CUSTOM ERROR MESSAGE
+		//JACKSON WILL TAKE THIS JAVA POJO AND CONVERT IT TO JSON AND SHOW US IN OUR REST CLIENT,
+		//OUR CUSTOM ERROR RESPONSE MESSAGE.
 	}
 	
+	//THIS IS GENERIC EXCEPTION HANDLER,this exception handler can catch any exception (catch all)
+	//THIS EXCEPTION HANDLER CAN TO HANDLE OR CATCH EVERY Exception except this CustomerNotFoundException
+	//example:when we enter-api/employee/dhsjdh then will be called this exception
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception exc) {
+		
+
+		CustomerErrorResponse error = new CustomerErrorResponse(
+											HttpStatus.BAD_REQUEST.value(),
+											exc.getMessage(),
+											System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
 	
-	
-	
+
 }
